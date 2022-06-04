b95f510ce0fa7636a4d8508c976418a68bd3adbf
umbat <farhanpratamax@gmail.com>
2022-06-04 22:49:27 +0700

    layouting week weather activity

---
+++ app/src/main/java/com/umbat/skripsi_weather_app/ui/WeekWeatherActivity.kt
@@ -0,0 +1,12 @@
+package com.umbat.skripsi_weather_app.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.umbat.skripsi_weather_app.R
+
+class WeekWeatherActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_week_weather)
+    }
+}

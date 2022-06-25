4641cb8af0b8f40458e375b6d096058b93892dcf
mfaqihrm <faqihss46@gmail.com>
2022-06-25 15:36:34 +0800

    tambah test

--- app/src/main/java/com/umbat/skripsi_weather_app/ui/home/HomeFragment.kt
+++ app/src/main/java/com/umbat/skripsi_weather_app/ui/home/HomeFragment.kt
@@ -9,7 +9,7 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.umbat.skripsi_weather_app.databinding.FragmentHomeBinding
 
-//test
+//testis
 
 class HomeFragment : Fragment() {
 

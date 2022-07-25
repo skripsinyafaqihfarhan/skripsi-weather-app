f9f60c292b0c76eafe7db265aec76fb6cecc0c9a
mfaqihrm <faqihss46@gmail.com>
2022-07-25 09:51:46 +0800

    Fixed userloc

--- app/src/main/java/com/umbat/skripsi_weather_app/data/local/entity/Userloc.kt
+++ app/src/main/java/com/umbat/skripsi_weather_app/data/local/entity/Userloc.kt
@@ -4,7 +4,7 @@ import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
-@Entity(tableName = "UserlocTable")
+@Entity(tableName = "userloc")
 data class Userloc (
     @PrimaryKey(autoGenerate = true)
     @ColumnInfo(name = "ID")
